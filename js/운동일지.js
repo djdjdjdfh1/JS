cc5b055c80852d657f3a0d9b87566c1f610952a9
Park Chan <qkrcks8028@gmail.com>
2023-03-17 12:16:25 +0900

    2032_JS

--- js/운동일지.js
+++ js/운동일지.js
@@ -1,3 +1,27 @@
-let back = document.querySelector("#routineBack");
+let idChest = document.querySelector("#routineChest")
+let idBack = document.querySelector("#routineBack");
+let idShoulder = document.querySelector("#routineShoulder")
 
-back.classList.toggle()
+let classChest = document.querySelector(".chest");
+let classBack = document.querySelector(".back");
+let classShoulder = document.querySelector(".shoulder");
+
+classChest.addEventListener("click", function() {
+    idBack.classList.add("none");
+    idShoulder.classList.add("none");
+    idChest.classList.remove("none");
+})
+classBack.addEventListener("click",function() {
+    idChest.classList.add("none");
+    idShoulder.classList.add("none");
+    idBack.classList.remove("none");
+})
+classShoulder.addEventListener("click",function() {
+    idChest.classList.add("none");
+    idBack.classList.add("none");
+    idShoulder.classList.remove("none");
+})
+// 초기값에서 클래스네임 설정 어떻게 할건가
+// 시작할떄 다 논으로 했다가 하나만 추가해주면 되지 
+//back.className = "none";
+//shoulder.className = "none";
